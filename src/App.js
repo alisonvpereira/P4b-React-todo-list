d4e7c5ed31e0bd0650449baa7e5b7e3b99098dae
alisonvpereira <alisonvpereira@gmail.com>
2020-09-12 11:16:43 +0800

    code starter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <h1>My todo list</h1>
+    <TodoItem todo="Finish Plus project"/>
+    <TodoItem todo="Feed cat"/>
+    <TodoItem todo="Be awesome"/>
+  </div>
   );
 }
 
