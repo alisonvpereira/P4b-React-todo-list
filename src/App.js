7d030b02bb31f52799dfd4b696ea825b90439af2
alisonvpereira <alisonvpereira@gmail.com>
2020-09-12 12:49:26 +0800

    toggle complete

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
   const completeTodo = (index) => {
     const newTodos = [...todos];
-    newTodos[index].isCompleted = true;
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
   };
 
